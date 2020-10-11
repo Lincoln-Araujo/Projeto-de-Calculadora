d948d4683bd55779e04c200f2e38ccd96ded897e
Lincoln Araujo <72479081+Lincoln-Araujo@users.noreply.github.com>
2020-10-11 16:02:31 -0300

    Add files via upload
    Segunda versão do meu primeiro código escrito em Python. Aplicando os conhecimentos que obtive até agora.

--- Calculadora.py
+++ Calculadora.py
@@ -1,60 +1,88 @@
 def boasVindas():
-    print("Calculadora de Operações Básicas\n")
-    print("(1) SOMA\n(2) SUBTRAÇÃO\n(3) MULTIPLICAÇÃO\n(4) DIVISÃO\n")
+    print("\n+ - * / Calculadora de Operações Básicas + - * /\n")
+    print("(1) SOMA\n(2) SUBTRAÇÃO\n(3) MULTIPLICAÇÃO\n(4) DIVISÃO\n") 
+         
+def calculadora():
+    operacao = input(" Qual tipo de operação? \n")
 
-def calculadora():    
-    operacao = int(input("Qual tipo de operação? \n"))
-    while operacao == 1 or 2 or 3 or 4:
-        if operacao == 1:
-            return soma()
-        elif operacao == 2:
-            return subtracao()
-        elif operacao == 3:
-            return multiplicacao()
-        elif operacao == 4:
-            return divisao()
+    listaDeStringsValidas = ["1", "2", "3", "4"]
+
+    if operacao in listaDeStringsValidas:
+
+        somar = "1"
+        subtrair = "2"
+        multiplicar = "3"
+        dividir = "4"
+
+        numero1 = entradaDeNumero(" Qual o primeiro número? \n", "\nDigite um número válido\n")
+        numero2 = entradaDeNumero(" Qual o segundo número? \n", "\nDigite um número válido\n")
+    
+        if operacao == somar:
+            soma(numero1, numero2)
+        elif operacao == subtrair:
+            subtracao(numero1, numero2)
+        elif operacao == multiplicar:
+            multiplicacao(numero1, numero2)
+        elif operacao == dividir:
+            divisao(numero1, numero2)
         else:
-            return mensagemDeErro()    
+            return mensagemDeErro() 
+    else:
+        return mensagemDeErro()
+
+def entradaDeNumero(perguntaPeloNumero, alertaDeNumeroInvalido):
+    x = None
+   
+    while True:
+        try:
+            x = float(input(perguntaPeloNumero))
+            break
+        except ValueError:
+            print(alertaDeNumeroInvalido)
+           
+    return x
                   
-def soma():
-    numero1 = float(input("Qual o primeiro número? \n"))
-    numero2 = float(input("Qual o segundo número? \n"))
+def soma(numero1, numero2):    
     resultado = numero1 + numero2
-    print("{} + {} = {}\n".format(numero1, numero2, resultado))
-    novaOperacao()
+    print("{} + {} = {}".format(numero1, numero2, resultado))
 
-def subtracao():
-    numero1 = float(input("Qual o primeiro número? \n"))
-    numero2 = float(input("Qual o segundo número? \n"))
+def subtracao(numero1, numero2):
     resultado = numero1 - numero2
-    print("{} - {} = {}\n".format(numero1, numero2, resultado))
-    novaOperacao()
+    print("{} - {} = {}\n".format(numero1, numero2, resultado))   
 
-def multiplicacao():
-    numero1 = float(input("Qual o primeiro número? \n"))
-    numero2 = float(input("Qual o segundo número? \n"))
+def multiplicacao(numero1, numero2):
     resultado = numero1 * numero2
-    print("{} * {} = {}\n".format(numero1, numero2, resultado))
-    novaOperacao()
+    print("{} * {} = {}\n".format(numero1, numero2, resultado))   
 
-def divisao():
-    numero1 = float(input("Qual o primeiro número? \n"))
-    numero2 = float(input("Qual o segundo número? \n"))
-    resultado = numero1 / numero2
-    print("{} / {} = {}\n".format(numero1, numero2, resultado))
-    novaOperacao()
+def divisao(numero1, numero2):
+    if numero2 == float(0):
+        print("\n!!! Não é possivel dividir {} por 0 !!!\n".format(numero1))
+        boasVindas()
+        calculadora()
+    else:
+        resultado = numero1 / numero2
+        print("{} / {} = {}\n".format(numero1, numero2, resultado))    
 
 def novaOperacao():
-    novoCalculo = int(input("Quer fazer outra operação? (1) SIM ; (2) NÃO\n"))
-    print("(1) SOMA\n(2) SUBTRAÇÃO\n(3) MULTIPLICAÇÃO\n(4) DIVISÃO\n")
-    while novoCalculo == 1:
-        return calculadora()       
+    novoCalculo = input("\nQuer fazer outra operação? (1) SIM ; (2) NÃO\n")
+    if novoCalculo == "1":
+        boasVindas()
+        calculadora() 
+        novaOperacao() 
+    elif novoCalculo == "2":
+        print("\nObrigado por utilizar meu programa\n")    
     else:
-        print("\nObrigado por utilizar meu programa\n")
+        mensagemDeErro2()
 
 def mensagemDeErro():
-    print("\nEscolha uma opção válida\n")  
-    return calculadora()
+    print("\n!!! Escolha uma opção válida !!!\n")
+    boasVindas() 
+    calculadora()
+    
+def mensagemDeErro2():
+    print("\n!!! Escolha uma opção válida !!!\n")
+    novaOperacao()
 
 boasVindas()
 calculadora()
+novaOperacao()
